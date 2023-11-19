d9d80ee54ff55c1e6fc4fa49263ca54986663a5e
secondary-jcav <secondary.jcav@gmail.com>
2023-11-19 19:52:44 +0100

    Script + basic file to test enpoint works

---
+++ test_agent.py
@@ -0,0 +1,16 @@
+import asyncio
+import content_generator
+from dotenv import find_dotenv, load_dotenv
+
+# Tests basic functionality
+_ = load_dotenv(find_dotenv())
+assistant = content_generator.ContentGenerator()
+
+FILE_SAMPLE = 'petstore_simple.json'
+
+with open(FILE_SAMPLE, 'r') as file:
+    data = file.read()
+    tests = asyncio.run(assistant.get_openai_response(data))
+    assert "describe" in tests
+    assert "it" in tests
+    print(tests)

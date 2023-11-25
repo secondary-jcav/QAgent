b6de9b9cf183cd2671507d57fc7d45e0db29ea74
secondary-jcav <secondary.jcav@gmail.com>
2023-11-25 15:48:23 +0100

    CHanged prompt

--- content_generator.py
+++ content_generator.py
@@ -8,14 +8,14 @@ class ContentGenerator:
     def __init__(self):
         self.client = AsyncOpenAI()
 
-    async def get_openai_response(self, prompt):
+    async def get_openai_response(self, prompt, framework):
         """
         Get a response from OpenAI's API.
         """
-        system_role = "You're an expert software engineer. You receive program documentation and provide Cypress tests to validate it" \
-                      "If you receive an API doc in the OpenAPI standard, you will provide functional API tests using cypress that cover all the endpoints detailed in the API doc." \
-                      "If you receive an html, you will provide E2E cypress tests that cover the locators and attributes present in the file." \
-                      "It's important that your response covers every endpoint or selector describen in the documentation you receive"
+        system_role = f"You're an expert software engineer. You receive program documentation and provide {framework} tests to validate it" \
+                      f"If you receive an API doc in the OpenAPI standard, you will provide functional API tests using {framework} that cover all the endpoints detailed in the API doc." \
+                      f"If you receive an html, you will provide E2E {framework} tests that cover the locators and attributes present in the file." \
+                      f"It's important that your response covers every endpoint or selector describen in the documentation you receive"
         try:
             completion = await self.client.chat.completions.create(
                 model="gpt-4",
@@ -27,7 +27,7 @@ class ContentGenerator:
                     },
                     {
                         "role": "user",
-                        "content": "For the provided content, respond with detailed cypress tests",
+                        "content": f"For the provided content, respond with detailed {framework} tests",
                     },
                     {
                         "role": "user",

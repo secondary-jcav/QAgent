336881e10a6cc4ffefa51ff93164bcdbdc17d309
secondary-jcav <secondary.jcav@gmail.com>
2024-04-07 14:27:12 +0200

    adding doc names as metadata

--- llm/bug_finder.py
+++ llm/bug_finder.py
@@ -5,9 +5,9 @@ from llama_index.core import (
 )
 from base.base_class import BaseClass
 
-prompt = f"In test_results.txt you'll find a test report containing one or more failing tests" \
+prompt = f"In test_report you'll find one or more failing tests" \
          f"First, identify what elements are involved in the assertion error that makes the test fail" \
-         f"Then look at git_log.txt and identify which changes dealt with those elements" \
+         f"Then look at git_changes and identify which changes dealt with those elements" \
          f"Report back with: the commit id of those changes and why you think they broke the tests"
 
 
@@ -23,12 +23,13 @@ class ReportChecker(BaseClass):
     def read_documents(path) -> list[Document]:
         return SimpleDirectoryReader(path).load_data()
 
-    def test_result_analysis(self, report, commits):
+    def test_result_analysis(self, report, commits) -> str:
         changes = self.build_document(commits)
+        changes.metadata = {"filename": "git_changes"}
         report = self.build_document(report)
+        report.metadata = {"filename": "test_report"}
         print("Looking for breaking changes")
         index = VectorStoreIndex.from_documents([changes, report])
         query_engine = index.as_query_engine()
         response = query_engine.query(prompt)
         return str(response)
-
